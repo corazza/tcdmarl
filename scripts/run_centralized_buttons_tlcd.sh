b9b20a20bff889d2736b713e9c40c66a7530e38e
Jan Corazza <corazzajan@gmail.com>
2024-05-22 22:13:30 +0200

    anonymized

--- scripts/run_centralized_buttons_tlcd.sh
+++ scripts/run_centralized_buttons_tlcd.sh
@@ -7,4 +7,4 @@ then
 fi
 
 # Run with TL-CD
-export PYTHONPATH="${PWD}" ; cd /home/jan.corazza/repos/temporal-causal-dmarl ; /usr/bin/env /home/jan.corazza/anaconda3/envs/tcdmarl/bin/python /home/jan.corazza/repos/temporal-causal-dmarl/tcdmarl/main.py --collection $1 --tlcd --experiment centralized_buttons --num-trials 10  --step-unit-factor 1000
+export PYTHONPATH="${PWD}" ; cd /home/user.name/repos/temporal-causal-dmarl ; /usr/bin/env /home/user.name/anaconda3/envs/tcdmarl/bin/python /home/user.name/repos/temporal-causal-dmarl/tcdmarl/main.py --collection $1 --tlcd --experiment centralized_buttons --num-trials 10  --step-unit-factor 1000
