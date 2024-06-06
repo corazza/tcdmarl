67fd301e97024cd22ed4f15691fb738c218e8fb3
Jan Corazza <corazzajan@gmail.com>
2024-06-06 13:28:33 +0200

    wip

---
+++ scripts/run_decentralized_buttons_tlcd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# $1 refers to the first command line argument
+if [ -z "$1" ]
+then
+  echo "No collection argument supplied"
+  exit 1
+fi
+
+# Run with TL-CD
+export PYTHONPATH="${PWD}" ; cd /home/jan.corazza/repos/temporal-causal-dmarl ; /usr/bin/env /home/jan.corazza/anaconda3/envs/tcdmarl/bin/python /home/jan.corazza/repos/temporal-causal-dmarl/tcdmarl/main.py --collection $1 --tlcd --experiment buttons --num-trials 30  --step-unit-factor 200
